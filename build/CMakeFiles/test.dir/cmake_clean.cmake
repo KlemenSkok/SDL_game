55d00694b328cb51a9e58b733af1e5f2f0b43ae6
Klemen Skok <klemen.skok1@gmail.com>
2024-02-17 23:51:45 +0100

    zdej pa dela perfecto mkm

--- build/CMakeFiles/test.dir/cmake_clean.cmake
+++ build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -1,12 +1,12 @@
 file(REMOVE_RECURSE
+  "C:/Users/Klemen/Desktop/test3/bin/test.exe"
+  "C:/Users/Klemen/Desktop/test3/bin/test.exe.manifest"
+  "C:/Users/Klemen/Desktop/test3/bin/test.pdb"
   "CMakeFiles/test.dir/src/main.cpp.obj"
   "CMakeFiles/test.dir/src/main.cpp.obj.d"
   "CMakeFiles/test.dir/src/objekt.cpp.obj"
   "CMakeFiles/test.dir/src/objekt.cpp.obj.d"
   "libtest.dll.a"
-  "test.exe"
-  "test.exe.manifest"
-  "test.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
